55c3126b484a106e9c1b85964b4629396ea4fd4d
yuru <leo32095@gmail.com>
2023-04-12 13:58:43 +0800

    led_pwm

--- pico/pwm.py
+++ pico/pwm.py
@@ -3,14 +3,14 @@ from time import sleep
 
 pwm = PWM(Pin(25))
 
-pwm.freq(40)
+pwm.freq(60)
 
 while True:
-    for duty in range(1000,20000,1):
+    for duty in range(1000, 60000, 100):
         pwm.duty_u16(duty)
         sleep(0.001)
-    sleep(0.3)
-    for duty in range(20000, 1000, -1):
+    sleep(0.1)
+    for duty in range(60000, 1000, -100):
         pwm.duty_u16(duty)
-        sleep(0.0001)
-    sleep(0.3)
+        sleep(0.001)
+    sleep(0.1)
